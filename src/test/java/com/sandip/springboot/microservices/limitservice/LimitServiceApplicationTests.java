e94f03aa9621cd49bbb80f5de8e3fd8cf6356c10
sandip5 <singhsandeep.singh97@gmail.com>
2021-02-09 19:05:54 +0530

    ADDED- LimitService Using CloudConfig And Git

---
+++ src/test/java/com/sandip/springboot/microservices/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandip.springboot.microservices.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
